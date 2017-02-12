6c67dd2305c790adcae88f1b698ae5ffd9670673
lbreguet <louis-w@live.com>
2017-02-11 19:09:42 -0500

    Ruby intro
    -review ruby

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,25 @@
+# frozen_string_literal: true
 require 'pry'
+def fizzbuzz(num)
+  fb = {
+    fizz: [],
+    buzz: [],
+    fizzbuzz: [],
+    other: []
+  }
 
-# Let `max_num` be the number that you want to count to.
+  for i in 1.upto(num) do
+    if (i % 3).zero? && (i % 5).zero?
+      fb[:fizzbuzz] << i
+    elsif (i % 3).zero?
+      fb[:fizz] << i
+    elsif (i % 5).zero?
+      fb[:buzz] << i
+    else
+      fb[:other] << i
+    end
+  end
+  puts fb
+end
+
+fizzbuzz(20)
